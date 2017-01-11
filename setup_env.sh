8a8e718964a6bbb79537a30fe441a5b47b52899a
Ubuntu <ubuntu@ip-172-31-21-182.us-west-2.compute.internal>
2017-01-11 09:11:00 +0000

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,11 @@
 # Download the images 
 wget http://press.liacs.nl/mirflickr/mirflickr25k/mirflickr25k.zip
 wget http://press.liacs.nl/mirflickr/mirflickr25k/mirflickr25k_annotations_v080.zip
+
+# Download the features. 
+
+wget http://www.cs.toronto.edu/~nitish/multimodal/flickr_data.tar.gz
+
+
+
+
