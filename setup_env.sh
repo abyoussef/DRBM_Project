8267ffb8f4bec123c7425ca6273da1fb58a9e091
Youssef <youssef949@hotmail.com>
2017-01-22 17:57:15 +0100

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,6 @@ wget http://press.liacs.nl/mirflickr/mirflickr25k/mirflickr25k.zip
 wget http://press.liacs.nl/mirflickr/mirflickr25k/mirflickr25k_annotations_v080.zip
 
 # Download the features. 
-
 wget http://www.cs.toronto.edu/~nitish/multimodal/flickr_data.tar.gz
 
 
